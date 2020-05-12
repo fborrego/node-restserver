b840de7734e6f6e34058aefd8c57c5a59dbc35ea
FranBorrego <fran.borrego83@gmail.com>
2020-05-12 12:22:39 +0200

    Variables de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://novemosenlaweb:5KrZ2RqprZVhMg3h@cluster0-ck9iq.mongodb.net/cafe'
+    urlDB = process.env.MONGO_uri;
 }
 
 process.env.URLDB = urlDB;

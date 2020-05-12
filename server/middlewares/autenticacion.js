be631463d94c2235fc90c9a5f69f7b8f6f12bcd8
FranBorrego <fran.borrego83@gmail.com>
2020-05-12 16:19:08 +0200

    Login normal sección 10

---
+++ server/middlewares/autenticacion.js
@@ -0,0 +1,57 @@
+const jwt = require('jsonwebtoken');
+
+/*
+    VERIFICAR TOKEN
+
+*/
+
+let verificaToken = (req, res, next) => {
+
+    let token = req.get('token'); // recibo el token desde el header
+    jwt.verify(token, process.env.SEMILLA, (err, decoded) => {
+
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'TOKEN no válido'
+                }
+            });
+        }
+
+        req.usuario = decoded.usuario;
+        next(); // Si no se ejecuta next, terminará la llamada y no continuará
+
+    });
+
+
+
+};
+
+/*
+    VERIFICAR TOKEN
+
+*/
+let verificaAdminRole = (req, res, next) => {
+
+    let usuario = req.usuario;
+
+    if (usuario.role === 'ADMIN_ROLE') {
+        next();
+    } else {
+        res.json({
+            ok: false,
+            err: {
+                message: 'El usuario no es administrador'
+            }
+        });
+    }
+
+
+};
+
+
+module.exports = {
+    verificaToken,
+    verificaAdminRole
+}
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -5,11 +5,14 @@ const _ = require('underscore');
 
 const Usuario = require('../models/usuario');
 
+const { verificaToken } = require('../middlewares/autenticacion');
+const { verificaAdminRole } = require('../middlewares/autenticacion');
+
 
 const app = express();
 
 
-app.get('/usuario', function(req, res) {
+app.get('/usuario', verificaToken, (req, res) => {
 
     let desde = req.query.desde || 0;
     desde = Number(desde);
@@ -40,7 +43,7 @@ app.get('/usuario', function(req, res) {
         })
 })
 
-app.post('/usuario', function(req, res) {
+app.post('/usuario', [verificaToken, verificaAdminRole], (req, res) => {
 
     let body = req.body;
 
@@ -69,7 +72,7 @@ app.post('/usuario', function(req, res) {
 })
 
 //ACTUALIZACIÓN DE UN REGISTRO
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdminRole], (req, res) => {
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'emal', 'img', 'role', 'estado']); //campos que sí se pueden actualizar
 
@@ -92,7 +95,7 @@ app.put('/usuario/:id', function(req, res) {
 
 })
 
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdminRole], (req, res) => {
     let id = req.params.id;
 
     //Eliminación física
